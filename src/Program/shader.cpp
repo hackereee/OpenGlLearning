c5af84c6b410bcf7dc45b8bd90a7d6d21ad5dedd
hackereee <cccscccs111@gmail.com>
2023-06-20 06:51:34 +0800

    更新着色器程序

--- src/Program/shader.cpp
+++ src/Program/shader.cpp
@@ -2,23 +2,42 @@
 
 using namespace std;
 
-Shader::Shader(char* vertexSourcePath, char* fragmentSourcePath){
-    string vertexCode;
-    string fragmentCode;
-    ifstream vShaderFile;
-    ifstream fShaderFile;
-    vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
-    fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
+Shader::Shader(char* vertexSourcePath, char* fragmentSourcePath) {
+	string vertexCode;
+	string fragmentCode;
+	ifstream vShaderFile;
+	ifstream fShaderFile;
+	vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
+	fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
 
-    try {
+	try {
+		vShaderFile.open(vertexCode);
+		fShaderFile.open(fragmentCode);
+		stringstream vShaderStream, fShaderStream;
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		vShaderFile.close();
+		fShaderFile.close();
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (ifstream::failure e) {
+		cout << "init shader source failed: " << endl;
+	}
 
-    }
-    catch (ifstream::failure e) {
-        cout << "init shader source failed: " << endl;
-    }
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
+
 }
 
-void Shader::use(){
+void Shader::compileAndLink(char* vertexCode, char* fragmentCode) {
+	unsigned int vertex, fragment;
+	int success;
+	char infoLog[512];
+
+}
+
+void Shader::use() {
 
 }
 
